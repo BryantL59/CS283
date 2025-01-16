2b5d28f613b2d33fe2998f0bf48f32b36a9008ef
Bryant Lin <bryantlin59@gmail.com>
2025-01-16 00:46:47 -0500

    finished no ec

--- 1-c-Refresher/stringfun.c
+++ 1-c-Refresher/stringfun.c
@@ -19,28 +19,27 @@ int setup_buff(char *buff, char *user_str, int len) {
         return -2;
     }
 
-    int user_len = 0;
-    int whitespace = 0;
+    int string_len = 0;
 
-    while (*user_str != '\0') {
-        if (user_len >= len) {
-            return -1;
+    while (*user_str != '\0' && string_len < len - 1) {
+        if (*user_str != ' ' || (string_len > 0 && *(buff + string_len - 1) != ' ')) {
+            *(buff + string_len) = *user_str;
+            string_len++;
         }
-
-        if (*user_str == ' ') {
-            if (!whitespace) {
-                buff[user_len++] = ' ';
-                whitespace = 1;
-            } 
-        } else {
-            buff[user_len++] = *user_str;
-            whitespace = 0;
-        }
-
         user_str++;
     }
 
-    return user_len; //for now just so the code compiles. 
+
+
+    for (int i = string_len; i < len - 1; i++) {
+        *(buff + i) = '.';
+    }
+
+     if (*user_str != '\0') {  
+        return -1;
+    }
+    
+    return string_len;
 }
 
 void print_buff(char *buff, int len) {
@@ -56,29 +55,49 @@ void usage(char *exename) {
 }
 
 int count_words(char *buff, int len, int str_len) {
-
     int count = 0;
-    int in_word = 0;
-
     int limit = len < str_len ? len : str_len;
 
     for (int i = 0; i < limit; i++) {
-        if (buff[i] == ' ') {
-            if (in_word) {
-                count++;
-                in_word = 0;
-            }
-        } else {
-            in_word = 1;
+        if (*(buff + i) != ' ' && (i == 0 || *(buff -1) == ' ')) {
+            count++;
         }
-    }
-
-    if (in_word) {
-        count++;
     }
 
     return count;
 }
+
+void reverse_string(char *buff, int len) {
+    for (int i = 0; i < len / 2; i++) {
+
+        char frontLetter = *(buff + i);
+        *(buff + i) = *(buff + len - i - 1);
+        *(buff + len - i - 1) = frontLetter;
+
+    }
+
+}
+
+void print_word(char *buff, int len) {
+    
+    int word_length = 0;
+    int count = 1;
+
+    for (int i = 0; i < len; i++) {
+
+        if (*(buff + i) != ' ' && *(buff + i) >= 32 && *(buff + i) <= 126) {
+            word_length++; 
+        } else {
+                printf("%d. %.*s (%d)\n", count, word_length, buff + i - word_length, word_length);
+                word_length = 0; 
+                count++;
+        }
+    }
+
+    printf("%d. %.*s (%d)\n", count, word_length, buff + len - word_length, word_length);
+}
+
+
 
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
 
@@ -146,34 +165,12 @@ int main(int argc, char *argv[]) {
             break;
 
         case 'r':
-            for (int i = 0; i < user_str_len / 2; i++) {
-                char temp = buff[i];
-                buff[i] = buff[user_str_len - i - 1];
-                buff[user_str_len - i - 1] = temp;
-            }
-
-            print_buff(buff, BUFFER_SZ);
+            reverse_string(buff, user_str_len);
             break;
 
-       case 'w':
-            int word_length = 0;
-
-            for (int i = 0; i < user_str_len; i++) {
-                if (buff[i] != ' ' && buff[i] >= 32 && buff[i] <= 126) {
-                    word_length++; 
-                } else {
-                    if (word_length > 0) {
-                        printf("%.*s (%d)\n", word_length, buff + i - word_length, word_length);
-                        word_length = 0; 
-                    }
-                }
-            }
-
-            if (word_length > 0) {
-                printf("%.*s (%d)\n", word_length, buff + user_str_len - word_length, word_length);
-            }
+        case 'w':
+            print_word(buff, user_str_len);
             break;
-
 
         //TODO:  #5 Implement the other cases for 'r' and 'w' by extending
         //       the case statement options
@@ -183,14 +180,16 @@ int main(int argc, char *argv[]) {
     }
 
     //TODO:  #6 Don't forget to free your buffer before exiting
-    free(buff);
     print_buff(buff, BUFFER_SZ);
+    free(buff);
     exit(0);
 }
 
-// TODO:  #7  Notice all of the helper functions provided in the 
-// starter take both the buffer as well as the length.  Why
-// do you think providing both the pointer and the length
-// is a good practice, after all we know from main() th
-
-//something2
+//TODO:  #7  Notice all of the helper functions provided in the 
+//          starter take both the buffer as well as the length.  Why
+//          do you think providing both the pointer and the length
+//          is a good practice, after all we know from main() that 
+//          the buff variable will have exactly 50 bytes?
+//  
+//          This is to ensure safe code execution, for example you can check if the length of the buff matches the length
+//          of the actual string or if it doesn't.
